9bfb04ee973a6778d3c7c20a811e1c6164c87588
Jaymin Raval <jayminraval57@gmail.com>
2025-10-30 00:48:09 +0530

    Update login page hide navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ProtectedRoute from "./components/ProtectedRoute";
 
+const AppLayout = ({ children }) => {
+  const location = useLocation();
+  // Hide navbar on login page
+  const hideNavbar = location.pathname === "/login";
+
+  return (
+    <div className="app-container">
+      {!hideNavbar && <Navbar />}
+      {children}
+      <Footer />
+    </div>
+  );
+};
+
 function App() {
   return (
     <Router>
-      <div className="app-container">
-        <Navbar />
-
+      <AppLayout>
         <Routes>
           {/* Public route */}
           <Route path="/login" element={<Login />} />
@@ -26,9 +38,7 @@ function App() {
             }
           />
         </Routes>
-
-        <Footer />
-      </div>
+      </AppLayout>
     </Router>
   );
 }
